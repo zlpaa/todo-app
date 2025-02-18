42e4f64f36e598b96951869cadf47c8acf2f9044
zalfadwihanifa <zalfadwi2007@gmail.com>
2025-02-18 08:48:21 +0700

    menambahkan kometar di database migrations create_items_table.php

--- database/migrations/2025_02_10_042342_create_items_table.php
+++ database/migrations/2025_02_10_042342_create_items_table.php
@@ -7,21 +7,30 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Menjalankan migrasi.
+     * Method ini digunakan untuk mendefinisikan struktur tabel yang akan dibuat di database.
      */
     public function up(): void
     {
+        // Membuat tabel 'items' di database
         Schema::create('items', function (Blueprint $table) {
+            // Kolom 'id' sebagai primary key untuk tabel 'items'. 
+            // Tipe data kolom 'id' adalah auto-incrementing integer.
             $table->id();
+
+            // Kolom 'timestamps' untuk menyimpan waktu pembuatan dan pembaruan data (created_at dan updated_at).
+            // Kolom ini akan otomatis diisi oleh Laravel.
             $table->timestamps();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Membalikkan migrasi (menghapus tabel).
+     * Method ini digunakan untuk menghapus tabel yang telah dibuat sebelumnya.
      */
     public function down(): void
     {
+        // Menghapus tabel 'items' jika tabel tersebut ada
         Schema::dropIfExists('items');
     }
 };

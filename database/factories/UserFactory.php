c4272e9f7fece7fb6fe00b499b98bfc504bfc66f
zalfadwihanifa <zalfadwi2007@gmail.com>
2025-02-18 08:30:40 +0700

    menambahkan komentar di database factories UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,37 +7,61 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
+ * Kelas ini adalah Factory untuk model User.
+ * Menghasilkan instance User dengan data palsu.
+ *
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
     /**
-     * The current password being used by the factory.
+     * Password yang saat ini digunakan oleh factory.
+     * Password ini diset hanya sekali untuk semua instance yang dihasilkan.
      */
     protected static ?string $password;
 
     /**
-     * Define the model's default state.
+     * Menentukan state default model.
      *
-     * @return array<string, mixed>
+     * Method ini mendefinisikan data default yang akan digunakan 
+     * untuk menghasilkan instance dari model User.
+     * 
+     * @return array<string, mixed> - Mengembalikan array yang berisi data 
+     * untuk dipakai dalam pembuatan model User.
      */
     public function definition(): array
     {
         return [
+            // Menghasilkan nama pengguna palsu menggunakan Faker
             'name' => fake()->name(),
+            
+            // Menghasilkan email unik yang aman dan palsu menggunakan Faker
             'email' => fake()->unique()->safeEmail(),
+            
+            // Menandakan waktu verifikasi email (sekarang)
             'email_verified_at' => now(),
+            
+            // Menggunakan password yang telah di-hash menggunakan Laravel Hash facade
+            // Jika $password belum diset, maka password default yang digunakan adalah "password"
             'password' => static::$password ??= Hash::make('password'),
+            
+            // Membuat token acak untuk keperluan "remember me" di aplikasi
             'remember_token' => Str::random(10),
         ];
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Menandakan bahwa alamat email model ini harus dianggap belum terverifikasi.
+     * 
+     * Metode ini digunakan untuk mengubah status verifikasi email menjadi null
+     * pada saat instance User dibuat.
+     * 
+     * @return static - Mengembalikan instance baru dari UserFactory dengan status email null.
      */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
+            // Mengatur atribut email_verified_at menjadi null
             'email_verified_at' => null,
         ]);
     }

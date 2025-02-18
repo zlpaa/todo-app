a6acaa358dc7f034691b9b26603312a24beadb5d
zalfadwihanifa <zalfadwi2007@gmail.com>
2025-02-18 08:40:04 +0700

    menambahkan komentar di database migrations create_task_lists_table.php

--- database/migrations/2025_01_30_031711_create_task_lists_table.php
+++ database/migrations/2025_01_30_031711_create_task_lists_table.php
@@ -7,22 +7,34 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
-     * Run the migrations.
+     * Menjalankan migrasi.
+     * Method ini digunakan untuk mendefinisikan struktur tabel yang akan dibuat di database.
      */
     public function up(): void
     {
+        // Membuat tabel 'task_lists' di database
         Schema::create('task_lists', function (Blueprint $table) {
+            // Kolom 'id' sebagai primary key untuk tabel 'task_lists'. 
+            // Tipe data kolom 'id' adalah auto-incrementing integer.
             $table->id();
+
+            // Kolom 'name' untuk menyimpan nama task list. 
+            // Kolom ini harus unik, artinya tidak boleh ada dua task list dengan nama yang sama.
             $table->string('name')->unique();
+
+            // Kolom 'timestamps' untuk menyimpan waktu pembuatan dan pembaruan data (created_at dan updated_at).
+            // Kolom ini akan otomatis diisi oleh Laravel.
             $table->timestamps();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * Membalikkan migrasi (menghapus tabel).
+     * Method ini digunakan untuk menghapus tabel yang telah dibuat sebelumnya.
      */
     public function down(): void
     {
+        // Menghapus tabel 'task_lists' jika tabel tersebut ada
         Schema::dropIfExists('task_lists');
     }
 };

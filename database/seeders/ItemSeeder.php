155c8bbe7be899facb4e8f3750c3e8ef311457b9
zalfadwihanifa <zalfadwi2007@gmail.com>
2025-02-18 09:27:32 +0700

    menambahkan komentar di databese seeders ItemSeeder.php

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -1,36 +1,43 @@
 <?php
 
-namespace Database\Seeders;
+namespace Database\Seeders; // Mendeklarasikan namespace untuk kelas seeder ini
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents; // Mengimpor WithoutModelEvents jika ingin menonaktifkan event pada model selama seeding (tidak digunakan dalam kode ini)
+use Illuminate\Database\Seeder; // Mengimpor kelas Seeder untuk mengeksekusi seeding data ke dalam database
 
+// Kelas ItemSeeder digunakan untuk melakukan seeding data pada tabel 'items'
 class ItemSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Method run digunakan untuk menambahkan data ke dalam tabel 'items'
      */
     public function run()
     {
-        \App\Models\Item::create([
-            'name' => 'Liburan',
+        // Menambahkan data ke dalam tabel 'items'
+        \App\Models\Item::create([ // Menggunakan model Item untuk membuat data baru
+            'name' => 'Liburan', // Menambahkan item 'Liburan'
         ]);
-        \App\Models\Item::create([
+        
+        \App\Models\Item::create([ // Menambahkan item 'Belajar'
             'name' => 'Belajar',
         ]);
-        \App\Models\Item::create([
+
+        \App\Models\Item::create([ // Menambahkan item 'Tugas'
             'name' => 'Tugas',
         ]);
-        \App\Models\Item::create([
+
+        \App\Models\Item::create([ // Menambahkan item 'Libur'
             'name' => 'Libur',
         ]);
-        \App\Models\Item::create([
+
+        \App\Models\Item::create([ // Menambahkan item 'Subang'
             'name' => 'Subang',
         ]);
-        \App\Models\Item::create([
+
+        \App\Models\Item::create([ // Menambahkan item 'Pakaian'
             'name' => 'Pakaian',
         ]);
-        // Tambahkan data lainnya
+
+        // Tambahkan data lainnya sesuai kebutuhan jika diperlukan
     }
-    
 }
